5d61ff4c6a6cb4d92860509cfca240cec5f97eba
limengke <752140235@qq.com>
2017-11-29 23:45:20 +0800

    2017/11/29的购买

---
+++ plan.js
@@ -0,0 +1,26 @@
+const Etf = require("./etf.js")
+module.export =  PlanList = [
+    {
+        time:"20171129",
+        info:{
+            buy:{
+                "000478":{
+                    num:2,
+                    value:2.3738
+                },
+                "001064":{
+                    num:1,
+                    value:0.7779
+                },
+                "003376":{
+                    num:1,
+                    value:0.9275
+                }
+            }
+        }
+        
+    }
+]
+    
+
+console.log(Etf)
